30bf66af25f254b65fdf0efab7c2efb47455b0d5
Maik Schneider <m.schneider@blueways.de>
2022-01-04 14:57:46 +0100

    feat: form migration sql

--- migration.sql
+++ migration.sql
@@ -1,2 +1,3 @@
 update tt_content set CType='ms_lebenswurzel_feature' where CType='lebenswurzel_base_feature';
 update tt_content set CType='ms_lebenswurzel_person' where CType='lebenswurzel_base_person';
+update tt_content set pi_flexform=REPLACE(pi_flexform, 'EXT:lebenswurzel_base', 'EXT:ms_lebenswurzel');
