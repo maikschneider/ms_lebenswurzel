12ef233b2dbfbc71adb3426e7db8bc12a97046e1
Maik Schneider <m.schneider@blueways.de>
2022-01-04 14:49:31 +0100

    fix: bw_placeholder_images version + js include, add migration.sql

---
+++ migration.sql
@@ -0,0 +1,2 @@
+update tt_content set CType='ms_lebenswurzel_feature' where CType='lebenswurzel_base_feature';
+update tt_content set CType='ms_lebenswurzel_person' where CType='lebenswurzel_base_person';
