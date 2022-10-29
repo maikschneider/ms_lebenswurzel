b55c976edad3de4022cd6b2b469d8fd2f75cff5d
Maik Schneider <m.schneider@blueways.de>
2022-10-29 16:05:47 +0200

    fix: use php8 in deployment, use gridelements tt_content schema changes

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,6 @@
 CREATE TABLE tt_content (
     style varchar(255) DEFAULT '' NOT NULL,
+	colPos       int(11) default '0' not null,
+	layout       varchar(255) default '' not null,
+	backupColPos int(11) default '-2' not null,
 );
